b4fd0c70dc3208c4d76f9f600305698a0d369717
WSL HOME <raulrecrec@gmail.com>
2026-06-20 14:25:13 +0200

    Add Animal base class
    - Created Animal class with protected type attribute.
    - Implemented Orthodox Canonical Form methods and getType.
    - Added virtual makeSound and virtual destructor for polymorphic behavior.

---
+++ ex00/Animal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/20 12:51:54 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/20 13:23:47 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <string>
+
+class Animal
+{
+	protected:
+		std::string	type;
+
+	public:
+		Animal(void);
+		Animal(const std::string &type);
+		Animal(const Animal &other);
+		Animal	&operator=(const Animal &other);
+		virtual	~Animal(void);
+
+		const std::string	&getType(void) const;
+		virtual void		makeSound(void) const;
+};
+
+#endif
