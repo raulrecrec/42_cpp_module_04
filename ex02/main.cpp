64962eee04d5be08994f1b4916c6cc2f37eb71be
rexposit <raulrecrec@gmail.com>
2026-06-21 20:53:50 +0200

    Update main for abstract Animal tests
    - Removed direct Animal instantiation from the test program.
    - Kept Dog and Cat creation through Animal pointers.
    - Added tests for polymorphic sounds, array deletion and deep copy behavior.

---
+++ ex02/main.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/20 12:51:56 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/21 20:53:02 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	const Animal	*j = new Dog();
+	const Animal	*i = new Cat();
+
+	std::cout << "----- Basic abstract class test -----" << std::endl;
+
+	j->makeSound();
+	i->makeSound();
+
+	delete j;
+	delete i;
+
+	std::cout << "----- Animal array test -----" << std::endl;
+
+	Animal	*animals[10];
+	int		index;
+
+	index = 0;
+	while (index < 10)
+	{
+		if (index < 5)
+			animals[index] = new Dog();
+		else
+			animals[index] = new Cat();
+		index++;
+	}
+
+	index = 0;
+	while (index < 10)
+	{
+		animals[index]->makeSound();
+		index++;
+	}
+
+	index = 0;
+	while (index < 10)
+	{
+		delete animals[index];
+		index++;
+	}
+
+	std::cout << "----- Deep copy test -----" << std::endl;
+
+	Dog	dog;
+	Dog	dog_copy(dog);
+	Dog	dog_assigned;
+
+	dog_assigned = dog;
+
+	Cat	cat;
+	Cat	cat_copy(cat);
+	Cat	cat_assigned;
+
+	cat_assigned = cat;
+
+	dog.makeSound();
+	dog_copy.makeSound();
+	dog_assigned.makeSound();
+
+	cat.makeSound();
+	cat_copy.makeSound();
+	cat_assigned.makeSound();
+
+	return (0);
+}
