2e3decb0bd3e1fb91235c8dcceee8bac67a01aa5
rexposit <raulrecrec@gmail.com>
2026-06-21 20:38:55 +0200

    Update Cat with Brain ownership
    - Added a private Brain pointer to the Cat class.
    - Allocated Brain during construction and deleted it during destruction.
    - Implemented deep copy behavior for Cat copy and assignment.

---
+++ ex01/Cat.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/20 12:51:45 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/21 20:27:13 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat(void)
+{
+	std::cout << "Cat default constructor called\n";
+	type = "Cat";
+	brain = new Brain();
+}
+
+Cat::Cat(const Cat &other) : Animal(other)
+{
+	std::cout << "Cat copy constructor called\n";
+	brain = new Brain(*other.brain);
+}
+
+Cat	&Cat::operator=(const Cat &other)
+{
+	if (this != &other)
+	{
+		Animal::operator=(other);
+		*this->brain = *other.brain;
+	}
+	return (*this);
+}
+
+Cat::~Cat(void)
+{
+	std::cout << "Dog destructor called\n";
+	delete brain;
+}
+
+void	Cat::makeSound(void) const
+{
+	std::cout << "Miau miau!\n";
+}
