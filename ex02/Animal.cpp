3b24aa287904779e62dc406faddc98bdbd32ea92
rexposit <raulrecrec@gmail.com>
2026-06-21 20:53:44 +0200

    Make Animal class abstract
    - Changed makeSound into a pure virtual function.
    - Prevented direct instantiation of the Animal base class.
    - Kept Animal usable as a polymorphic base class for Dog and Cat.

---
+++ ex02/Animal.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/20 12:51:52 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/21 20:48:21 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include <iostream>
+
+Animal::Animal(void)
+{
+	std::cout << "Animal default constructor called\n";
+	type = "Animal";
+}
+
+Animal::Animal(const std::string &type)
+{
+	std::cout << "Animal type constructor called\n";
+	this->type = type;
+}
+
+Animal::Animal(const Animal &other)
+{
+	std::cout << "Animal copy constructor called\n";
+	*this = other;
+}
+
+Animal	&Animal::operator=(const Animal &other)
+{
+	if (this != &other)
+		this->type = other.type;
+	return (*this);
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Animal destructor called\n";
+}
+
+const std::string	&Animal::getType(void) const
+{
+	return (type);
+}
